25781ebcb6eed36bdf81cd055ab40579751f312f
Darren Evans <darrenrevans10@gmail.com>
2018-01-11 16:56:03 -0500

    updated files

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendData = [
+  {
+    name: 'Francis Underwood',
+    profilePic: 'http://www.houseofcardsepisodes.com/wp-content/uploads/2015/01/maxresdefault.jpg',
+    scores: [
+      "5",
+      "3",
+      "4",
+      "3",
+      "4",
+      "5",
+      "5",
+      "4",
+      "2",
+      "5"
+    ]
+  }
+]
+
+module.exports = friendData;
