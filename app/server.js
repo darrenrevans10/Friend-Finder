2a86eeadec8e31e3ee608ded9f487c1aa5371a06
Darren Evans <darrenrevans10@gmail.com>
2018-01-11 16:58:16 -0500

    updated files

--- app/server.js
+++ app/server.js
@@ -7,8 +7,8 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
 
 
 app.listen(PORT, function() {
